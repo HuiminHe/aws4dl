2b502e80696367363cf74d14c3fc66d1497b7526
Ubuntu <ubuntu@ip-172-31-29-171.us-west-2.compute.internal>
2017-02-15 19:22:53 +0000

    update bashrc.bak

---
+++ cuda.sh
@@ -0,0 +1,5 @@
+wget https://developer.nvidia.com/compute/cuda/8.0/prod/local_installers/cuda-repo-ubuntu1604-8-0-local_8.0.44-1_amd64-deb
+sudo dpkg -i cuda-repo-ubuntu1604-8-0-local_8.0.44-1_amd64-deb
+rm cuda-repo-ubuntu1604-8-0-local_8.0.44-1_amd64-deb
+sudo apt-get update
+sudo apt-get install -y cuda
