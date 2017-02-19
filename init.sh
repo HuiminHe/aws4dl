8fe23510a9b17f3370186f13bc90fdcaf7648d67
Ubuntu <ubuntu@ip-172-31-29-203.us-west-2.compute.internal>
2017-02-19 00:06:29 +0000

    add torch.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install -y build-essential git python3-pip libfreetype6-dev libxft-dev libncurses-dev libopenblas-dev gfortran python3-matplotlib libblas-dev liblapack-dev libatlas-base-dev python3-dev python3-pydot linux-headers-generic linux-image-extra-virtual unzip python3-numpy swig python3-pandas python3-sklearn unzip wget git pkg-config zip g++ zlib1g-dev libcurl3-dev jupyter
+sudo apt-get install -y build-essential git python3-pip libfreetype6-dev libxft-dev libopenblas-dev gfortran python3-matplotlib libblas-dev liblapack-dev libatlas-base-dev python3-dev python3-pydot linux-headers-generic linux-image-extra-virtual unzip python3-numpy swig python3-pandas python3-sklearn unzip wget git pkg-config zip g++ zlib1g-dev libcurl3-dev gcc valgrind emacs24
+
 sudo pip install -U pip
 sudo mv dropbox_uploader.sh ~/.dbx.sh
 sudo mv .emacs ~/.emacs
