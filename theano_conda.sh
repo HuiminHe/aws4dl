ca5d45dc8fbd70e348f28f3986673c8246457dd7
Ubuntu <ubuntu@ip-172-31-8-34.us-west-2.compute.internal>
2017-03-20 15:29:21 +0000

    update

--- theano_conda.sh
+++
@@ -1,5 +0,0 @@
-mkdir downloads
-cd downloads
-wget -0 Anaconda3.sh https://repo.continuum.io/archive/Anaconda3-4.2.0-Linux-x86_64.sh
-bash Anaconda3.sh -b -p ~/usr/local/bin/anaconda3
-'export PATH="~/usr/local/bin/anaconda3:$PATH"' >> .bashrc
