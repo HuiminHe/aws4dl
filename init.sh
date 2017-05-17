db38d9ac81fe63e19f021953c8633a4d55a3a075
Hugh <hh162@duke.edu>
2017-05-17 00:16:44 -0400

    fix incorrect install

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install -y build-essential git python3-pip libfreetype6-dev libxft-dev libopenblas-dev gfortran python3-matplotlib libblas-dev liblapack-dev libatlas-base-dev python3-dev python3-pydot linux-headers-generic linux-image-extra-virtual unzip python3-numpy swig python3-pandas python3-sklearn unzip wget git pkg-config zip g++ zlib1g-dev libcurl3-dev gcc valgrind emacs24 virtualenv cmake
+sudo apt-get install -y build-essential git python3-pip libfreetype6-dev libxft-dev libopenblas-dev gfortran python3-matplotlib libblas-dev liblapack-dev libatlas-base-dev python3-dev python3-pydot linux-headers-generic linux-image-extra-virtual unzip python3-numpy swig python3-pandas python3-sklearn unzip wget git pkg-config zip g++ zlib1g-dev libcurl3-dev gcc valgrind emacs24 virtualenv cmake --upgrade
+sudo apt-get install -f
 sudo apt-get clean
 sudo apt-get autoremove
 
