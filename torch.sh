b79f49051a55bf8b83d853d7baea0750c4273ce4
Ubuntu <ubuntu@ip-172-31-39-163.us-west-2.compute.internal>
2017-05-17 04:14:36 +0000

    update

--- torch.sh
+++
@@ -1,7 +0,0 @@
-if [ ! -f /home/ubuntu/torch ]; then
-    git clone https://github.com/torch/distro.git ~/torch --recursive
-fi
-cd ~/torch; bash install-deps;
-./install.sh
-source ~/.bashrc
-echo "Torch is configured"
