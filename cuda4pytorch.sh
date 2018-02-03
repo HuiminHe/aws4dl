4f6c757d88e99818c9cae489595df19c12653592
Ubuntu <ubuntu@ip-172-31-26-103.us-west-2.compute.internal>
2018-02-03 21:03:14 +0000

    update cuda support to 9.5

--- cuda4pytorch.sh
+++ cuda4pytorch.sh
@@ -1,8 +1,7 @@
 # nvida-driver
-wget http://us.download.nvidia.com/tesla/375.51/nvidia-driver-local-repo-ubuntu1604_375.51-1_amd64.deb
-sudo dpkg -i nvidia-driver-local-repo-ubuntu1604_375.51-1_amd64.deb
+wget http://us.download.nvidia.com/tesla/390.12/nvidia-diag-driver-local-repo-ubuntu1604-390.12_1.0-1_amd64.deb
+sudo dpkg -i nvidia-diag-driver-local-repo-ubuntu1604-390.12_1.0-1_amd64.deb
 sudo apt-get update
 sudo apt-get -y install cuda-drivers
-
 echo "Reboot required."
 
