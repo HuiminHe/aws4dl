a31762d6ce6f5547fc5c323a60cc1612c1078fcd
Ubuntu <ubuntu@ip-172-31-14-24.us-west-2.compute.internal>
2018-03-06 13:48:19 +0000

    fix bug of connection

--- python.sh
+++ python.sh
@@ -3,7 +3,7 @@ sudo -H pip3 install --upgrade pip
 sudo -H pip3 install numpy scipy matplotlib jupyter ipython
 jupyter notebook --generate-config -y
 cp jupter_notebook_config.py ~/.jupyter/
-mkdir ~/.ssl
-cd ~/.ssl
-sudo openssl req -x509 -nodes -days 365 -newkey rsa:1024 -keyout "cert.key" -out "cert.pem" -batch
+mkdir ~/ssl
+cd ~/ssl
+sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout cert.pem -out cert.pem
 cd ~
